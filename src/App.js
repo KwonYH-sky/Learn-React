f937c647c5f9f04ec50a9abbc367cccd132b381b
KwonYH <yhoung11@gmail.com>
2024-04-12 01:24:58 +0900

    Event

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function Header(props) {
   console.log('props', props.title);
   return (
     <header>
-      <h1><a href='/'>{props.title}</a></h1>
+      <h1><a href='/' onClick={(event) => {
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
     </header>
   );
 }
@@ -16,7 +19,10 @@ function Nav(props) {
   const lis = [];
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}><a id={t.id} href={'/read/' + t.id} onClick={event => {
+      event.preventDefault();
+      props.onChangeMode(event.target.id);
+    }}>{t.title}</a></li>)
   }
 
   return (
@@ -47,8 +53,12 @@ function App() {
 
   return (
     <div>
-      <Header title="Web"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="Web" onChangeMode={() => {
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
